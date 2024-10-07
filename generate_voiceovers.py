7b6173d6097ca3dcf1e2eb8345b86c900f0a63f8
MohammedHamzaMalik <m.h.m.i.malik@gmail.com>
2024-10-07 16:08:12 -0400

    created a function for generating the voiceovers

---
+++ generate_voiceovers.py
@@ -0,0 +1,10 @@
+from script_processing import processed_script
+from gtts import gTTS
+
+def generate_voiceover(text, language='en', output_file='voiceover.mp3'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save(output_file)
+    print(f"Voiceover saved as {output_file}")
+
+for i, sentence in enumerate(processed_script):
+    generate_voiceover(sentence, language='en', output_file=f'voiceover_scene_{i+1}.mp3')
