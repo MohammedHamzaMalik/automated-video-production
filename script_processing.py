5d148f50a0e7646ce7da3b24a620d20949767466
MohammedHamzaMalik <m.h.m.i.malik@gmail.com>
2024-10-07 16:07:49 -0400

    created a function for processing the script

---
+++ script_processing.py
@@ -0,0 +1,13 @@
+import nltk
+nltk.download('punkt')  # Download punkt for sentence tokenization
+nltk.download('punkt_tab')
+from nltk.tokenize import sent_tokenize
+
+def process_script(script_input):
+    sentences = sent_tokenize(script_input)
+    cleaned_sentences = [sentence.strip() for sentence in sentences]
+    return cleaned_sentences
+
+script = "Artificial intelligence is revolutionizing industries. It has applications in healthcare, finance, and education."
+processed_script = process_script(script)
+print(processed_script)
